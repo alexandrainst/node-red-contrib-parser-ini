a4ca60dbe2b7de1b5a4f392205e5139a1c4ed2d5
Alexandre Alapetite <alexandre.alapetite@alexandra.dk>
2020-05-20 18:57:23 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # node-red-contrib-parser-ini
-Node-RED node to parser/serialize INI configuration files
+[Node-RED](https://nodered.org) node to parser/serialize [INI](https://en.wikipedia.org/wiki/INI_file) configuration files.
+
+Based on the [isaacs/ini](https://github.com/npm/ini) library.
+
+License: [MIT](LICENSE.md), 2020.
+
+Originally made by [Alexandre Alapetite](https://alexandra.dk/alexandre.alapetite) at the [Alexandra Institute](https://alexandra.dk).
---
+++ parser-ini.js
@@ -0,0 +1,40 @@
+module.exports = function (RED) {
+	"use strict";
+	const ini = require('ini');
+	function ParserIniNode(n) {
+		RED.nodes.createNode(this, n);
+		this.property = n.property || 'payload';
+		const node = this;
+		this.on('input', function (msg) {
+			let value = RED.util.getMessageProperty(msg, node.property);
+			if (value !== undefined) {
+				if (typeof value === 'string') {
+					try {
+						value = ini.parse(value);
+						RED.util.setMessageProperty(msg, node.property, value);
+						node.send(msg);
+					} catch (e) {
+						node.error(e.message, msg);
+					}
+				} else if (typeof value === 'object') {
+					if (!Buffer.isBuffer(value)) {
+						try {
+							value = ini.stringify(value);
+							RED.util.setMessageProperty(msg, node.property, value);
+							node.send(msg);
+						} catch (e) {
+							node.error(RED._('parser-ini.errors.dropped-error'));
+						}
+					} else {
+						node.warn(RED._('parser-ini.errors.dropped-object'));
+					}
+				} else {
+					node.warn(RED._('parser-ini.errors.dropped'));
+				}
+			} else {
+				node.send(msg);	// If no payload - just pass it on.
+			}
+		});
+	}
+	RED.nodes.registerType('parser-ini', ParserIniNode);
+};
